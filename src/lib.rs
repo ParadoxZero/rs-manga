223383721d83d1b3f7b8906cc4f01fc9ce640284
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-09-14 23:15:39 +0530

    Fixed commandline parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,47 @@
-mod utilities;
 mod commands;
+mod utilities;
 
-use crate::utilities::commandline_parser;
+use commands::basic_commands;
+use utilities::commandline_parser;
 
 pub fn process_comandline(args: Vec<String>) {
     let (options, switches, commands) = commandline_parser::parse_input(args);
 
-    
+    dbg_print_params(options, switches, &commands);
+
+    if let Some(command) = commands.first().to_owned() {
+        handle_command(command);
+    } else {
+        basic_commands::help();
+    }
+}
+
+fn handle_command(command: &String) {
+    let is_cmd = |y: &str| -> bool {
+        return command.eq_ignore_ascii_case(y);
+    };
+
+    if is_cmd("help") {
+        basic_commands::help();
+    } else if is_cmd("search") {
+    } else if is_cmd("latest") {
+    } else {
+        // Assume rest of the words are manga title
+        println!("Finding details of manga - \"{command}\" ");
+    }
+}
+
+fn dbg_print_params(
+    options: std::collections::HashMap<String, String>,
+    switches: Vec<String>,
+    commands: &Vec<String>,
+) {
     if cfg!(debug_assertions) {
         println!();
-        println!("Input parameters -");
-        println!("Options   - {:?}", options);
-        println!("Switches  - {:?}", switches);
-        println!("Commands  - {:?}", commands);
+        println!("[Debug Info] Input parameters -");
+        println!("[Debug Info] Options   - {:?}", options);
+        println!("[Debug Info] Switches  - {:?}", switches);
+        println!("[Debug Info] Commands  - {:?}", commands);
         println!();
-    }
-
-    match commands.first() {
-        _ => commands::basic_commands::help()
     }
 }
--- src/utilities/commandline_parser.rs
+++ src/utilities/commandline_parser.rs
@@ -1,23 +1,23 @@
 use std::{collections::HashMap, str::FromStr};
 
-
 pub fn parse_input(args: Vec<String>) -> (HashMap<String, String>, Vec<String>, Vec<String>) {
     let mut options: HashMap<String, String> = HashMap::new();
     let mut switches: Vec<String> = Vec::new();
-    let mut commands:Vec<String> = Vec::new();
+    let mut commands: Vec<String> = Vec::new();
     for arg in args {
         if arg.starts_with("--") {
             let values = arg.replace("--", "");
-            let values = match values.split_once('=') {
-                None => values,
-                Some(a) => (FromStr::from_str(a.0.clone()).expect("Failed"), FromStr::from_str(a.1.clone()).expect("Failed"))
+            let values: (String, String) = match values.split_once('=') {
+                None => (values, String::new()),
+                Some(a) => (
+                    FromStr::from_str(a.0.clone()).expect("Failed to convert to string"),
+                    FromStr::from_str(a.1.clone()).expect("Failed to convert to string"),
+                ),
             };
-            options.insert();
-        }
-        else if arg.starts_with("-") {
+            options.insert(values.0, values.1);
+        } else if arg.starts_with("-") {
             switches.push(arg.replace("-", ""));
-        }
-        else {
+        } else {
             commands.push(arg);
         }
     }
